f78c8731c773a25fa69cd083dbf15288effa2c0c
matheussouzalima <matholaslima@gmail.com>
2022-09-25 11:55:14 -0300

    Função mostar array na tela

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Chute from "./Chute";
 import palavras from "./palavras"
 
 export default function App() {
-    let [estado, setEstado] = useState(0);
-    let imagem = `./assets/forca${estado}.png`
+    let imagem = `./assets/forca${0}.png`
     let alfabeto1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     let [disabled,SetDisabled] = useState(true);
     let [palavra,setPalavra] = useState("");
-    console.log(palavra)
+    let [ArrayTela,setArrayTela] = useState();
+    //let array = [...palavra];
+    
     return (
         <div className="App">
             <div className='Forca'>
                 <img src={imagem} alt="Forca" />
-                <button onClick={() => handleOnClick(setPalavra,SetDisabled)} disabled={!disabled} className="escolher"> Escolher Palavra </button>
+                <div className="Direita">
+                    <button onClick={() => handleOnClick(setArrayTela,setPalavra,SetDisabled)} disabled={!disabled} className="escolher"> Escolher Palavra </button>
+                    <h1 className="palavra">{ArrayTela}</h1>
+                </div>
             </div>
             <div className="Letras">
                 {
@@ -28,13 +32,23 @@ export default function App() {
     )
 }
 
-function handleOnClick(setPalavra,SetDisabled,){
-    setPalavra(randomizaPalavra());
+function handleOnClick(setArrayTela,setPalavra,SetDisabled){
     SetDisabled(false);
+    arrayNaTela(setArrayTela,randomizaPalavra(),setPalavra);
 }
 
 function randomizaPalavra(){
     palavras.sort(() => Math.random() - 0.5);
 
-    return "água"//palavras[0];
+    return palavras[0];
+}
+
+function arrayNaTela(setArrayTela,palavra,setPalavra){
+    let aux = [];
+    for(let i = 0; i<palavra.length;i++){
+        aux.push('_');
+    }
+
+    setArrayTela(aux);
+    setPalavra(palavra)
 }
--- src/Letras.js
+++ src/Letras.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export default function Letras(props){
+    let [isDisabled,setIsDisabled] = useState(false)
     return (
         <div className="Letra ">
-            <button onClick={(e) => handleClick(e)} className="disabled" disabled={props.disabled} >{props.letra}</button>
+            <button 
+                onClick={(e) => handleClick(e,props.palavra,setIsDisabled)} 
+                className={(props.disabled === false && isDisabled === false) ? "letra" : "disabled"}
+                disabled={(props.disabled === false && isDisabled === false) ? false : true} >
+                    {props.letra}
+            </button>
         </div>
     )
 }
 
-function handleClick(e){
+function handleClick(e,palavra,setIsDisabled){
+    setIsDisabled(true)
     console.log(e.target.outerText);
 }
